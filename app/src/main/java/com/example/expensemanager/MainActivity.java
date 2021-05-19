d920ca273dcaa3d099e5f180d63dffd9dcf5b85e
KiranBendkoli1 <bendkolikiran.com>
2021-05-19 21:42:18 +0530

    created login and signup page

---
+++ app/src/main/java/com/example/expensemanager/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.expensemanager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    EditText email,password;
+    Button loginBtn;
+    TextView forgetPassword, signupHere;
+    FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.login);
+        forgetPassword = findViewById(R.id.forgotPassword);
+        signupHere = findViewById(R.id.signUpHere);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailS = email.getText().toString().trim();
+                String passwordS = password.getText().toString().trim();
+                if(emailS.isEmpty()){
+                    email.setError("Email Required");
+                    return;
+                }
+                if(passwordS.isEmpty()){
+                    password.setError("Password Required");
+                    return;
+                }
+                firebaseAuth.signInWithEmailAndPassword(emailS,passwordS).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(),"Login Successful",Toast.LENGTH_SHORT);
+                            Intent i = new Intent(getApplicationContext(),Home.class);
+                            startActivity(i);
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(),"Login Failed",Toast.LENGTH_SHORT);
+                        }
+                    }
+                });
+            }
+        });
+        signupHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), RegistrationActivity.class);
+                startActivity(i);
+            }
+        });
+        forgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Password.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/expensemanager/RegistrationActivity.java
@@ -0,0 +1,73 @@
+package com.example.expensemanager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.SingleLineTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    EditText email,password;
+    Button regBtn;
+    TextView signIn;
+    FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        email = findViewById(R.id.email_reg);
+        password = findViewById(R.id.password_reg);
+        regBtn = findViewById(R.id.register);
+        signIn = findViewById(R.id.signInHere);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailS = email.getText().toString();
+                String passwordS = password.getText().toString();
+                if(emailS.isEmpty()){
+                    email.setError("Email Required");
+                    return;
+                }
+                if(passwordS.isEmpty()){
+                    password.setError("Password Required");
+                    return;
+                }
+                firebaseAuth.createUserWithEmailAndPassword(emailS,passwordS).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(),"Registration Complete",Toast.LENGTH_SHORT);
+                            Intent i = new Intent(getApplicationContext(),Home.class);
+                            startActivity(i);
+
+                        }else{
+                            Toast.makeText(getApplicationContext(),"Registration Failed",Toast.LENGTH_SHORT);
+                        }
+                    }
+                });
+            }
+        });
+
+    }
+
+
+
+
+
+}
